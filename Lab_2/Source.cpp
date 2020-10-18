f00d6c6f620d6e5d8da3e4cf455026cad5719830
ShasOKais <emil49@yandex.ru>
2020-10-18 23:51:17 +0500

    Четвертый шаг: Определение классов - наследников; Какие вызываются конструкторы и деструкторы.

--- Lab_2/Source.cpp
+++ Lab_2/Source.cpp
@@ -56,12 +56,41 @@ void Darkness::reset() {
 	y = 0;
 }
 
-int main() {
-	// Объекты создаются в куче
-	Darkness *Ford = new Darkness(10, 20);
-	Ford->reset(); // для корретности вывода координат
-	Ford->move(1, 2); // можно обратиться к методу класса
+class ColoredDarkness : public Darkness { // класс - потомок наследуется от класса Darkness
+protected:
+	int color; // атрибуты
+public: // спецификатор доступа
+	ColoredDarkness() : Darkness() { // конструктор базового класса
+		cout << "ColoredDarkness()\n";
+		color = 0;
+	}
+	ColoredDarkness(int x, int y, int color) : Darkness(x, y) { // конструктор c параментрами с параметрами Darkness
+		cout << "ColoredDarkness(int x, int y)\n";
+		// -> обращение к члену структуры
+		this->color = color; // this - указатель на текущий объект данного класса
+	}
+	ColoredDarkness(const ColoredDarkness& here) { // копирующий конструктор
+		// Переносит все свойства из объекта которое передали
+		cout << "ColoredDarkness(const ColoredDarkness &here)\n";
+		color = here.color;
+		x = here.x;
+		y = here.y;
+	}
+	~ColoredDarkness() { // деструктор
+		/*
+		  метод класса, служащий для
+		  деинициализации объекта (освобождение памяти)
+		*/
+		cout << "~ColoredDarkness() " << x << " " << y << " Color = " << color << endl;
+	}
+	void change_color(int new_color) { // метод класса
+		color = new_color;
+	}
+};
 
+int main() {
+
+	ColoredDarkness *Ford = new ColoredDarkness(1, 2, 42);
 	delete Ford;
 
 	return 0;
